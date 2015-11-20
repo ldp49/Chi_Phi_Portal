e56864814069c4600e31552390d0de02779fd6f1
Leopoldo Pena <leodpena@gmail.com>
2015-11-20 12:44:44 -0500

    generate controller for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static/hello'
+
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'posts#index'
+  root 'static#hello'
 
 
   # Example of regular route:
